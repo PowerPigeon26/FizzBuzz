f470049525387b37461fa9dccd9c8a97dbb8e9b9
Maverick Grayer <maverick72032@gmail.com>
2023-11-29 10:46:51 -0500

    FizzBuzz challenge completed in VScode

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿void FizzBuzz(int input)
+{
+    string fizzBuzz = "";
+    if (input % 3 == 0)
+    {
+        fizzBuzz = fizzBuzz + "fizz";
+    }
+    if (input % 5 == 0)
+    {
+        fizzBuzz = fizzBuzz + "buzz";
+    }
+    Console.WriteLine(fizzBuzz);
+}
+
+Console.WriteLine("Give a positive integer: (Ex. 1, 3, 65) ");
+int number;
+while (!int.TryParse(Console.ReadLine(), out number) || !(number > 0))
+{
+    Console.WriteLine("Not a valid positive integer, try again.");
+}
+
+FizzBuzz(number);
